e49debf50745c2bb9bb981e8491e1473ea87ec5a
Object NSS <agafonov.egorushka@gmail.com>
2024-09-27 19:17:28 +0300

    add changes

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Запуск скриптов в фоновом режиме
-python parsers/main.py &
-python parsers/bybit.py &
-python parsers/bybit_ws.py &
-python parsers/changers.py &
-python parsers/search.py &
-python bot.py &
+venv/bin/python parsers/main.py &
+venv/bin/python parsers/bybit.py &
+venv/bin/python parsers/bybit_ws.py &
+venv/bin/python parsers/changers.py &
+venv/bin/python parsers/search.py &
+venv/bin/python bot.py &
 
 # Ожидание завершения всех фоновых процессов
 wait
